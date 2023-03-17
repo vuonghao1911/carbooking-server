c626ea644066002fa45de9122b48fa1fdd623ed4
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2023-03-17 15:34:28 +0700

    fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+chmod +x ~/.nvm/nvm.sh
+source ~/.bashrc
+
 
 nvm install 16
-
+nvm use 16
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/car-booking"
 if [ -d "$DIR" ]; then
