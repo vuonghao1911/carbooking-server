9096670f1e2d77c882270d7500b6ad32a54b4082
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2023-03-17 13:39:24 +0700

    fix

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,13 +6,13 @@ sudo chmod -R 777 /home/ec2-user/car-booking
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/car-booking
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+# #add npm and node to path
+# export NVM_DIR="$HOME/.nvm"	
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-#install node modules
-npm install
-npm i -g pm2
-#start our node app in the background
+# #install node modules
+# npm install
+# npm i -g pm2
+# #start our node app in the background
 pm2 restart index > app.out.log 2> app.err.log < /dev/null & 
